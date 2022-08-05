f1d8e958bc71bc17651657556a4a8ab60a3f4052
electrovir <electrovir@users.noreply.github.com>
2022-08-05 18:32:57 +0000

    add script to combine all challenges

--- packages/scripts/src/file-paths.ts
+++ packages/scripts/src/file-paths.ts
@@ -1,11 +1,20 @@
 import {dirname, join} from 'path';
 
 export const repoRootDirPath = dirname(dirname(dirname(__dirname)));
-export const challengesSrcDirPath = join(repoRootDirPath, 'packages', 'challenges', 'src');
+export const challengesPackageSrcDirPath = join(repoRootDirPath, 'packages', 'challenges', 'src');
 
-export const baseChallengeFilePath = join(challengesSrcDirPath, 'vir-put-date-here.element.ts');
+export const baseChallengeFilePath = join(
+    challengesPackageSrcDirPath,
+    'vir-put-date-here.element.ts',
+);
 export const baseChallengeTestFilePath = join(
-    challengesSrcDirPath,
+    challengesPackageSrcDirPath,
     'vir-put-date-here.element.test.ts',
 );
-export const actualChallengeFilesDirPath = join(challengesSrcDirPath, 'challenges');
+export const actualChallengeFilesDirPath = join(challengesPackageSrcDirPath, 'challenges');
+
+export const allChallengesFilePath = join(
+    challengesPackageSrcDirPath,
+    'view-challenges-app',
+    'all-challenges.ts',
+);
