9b112412ba84277683a5b7d33483e79be30ac9c1
electrovir <electrovir@users.noreply.github.com>
2022-08-09 21:54:38 +0000

    August 5th challenge

---
+++ packages/challenges/src/challenges/vir-2022-08-05.element.test.ts
@@ -0,0 +1,47 @@
+import {assert, fixture as renderFixture} from '@open-wc/testing';
+import {html} from 'element-vir';
+import {clickElement} from '../test-helpers/interactions';
+import {queryByTestId} from '../test-helpers/test-id';
+import {assertNotNullish, typedAssertInstanceOf} from '../test-helpers/typed-assert';
+import {Vir_2022_08_05} from './vir-2022-08-05.element';
+
+describe(Vir_2022_08_05.tagName, () => {
+    async function renderElement() {
+        return await renderFixture(html`<${Vir_2022_08_05}></${Vir_2022_08_05}>`);
+    }
+
+    async function getLoginFormElements() {
+        const rendered = await renderElement();
+        const usernameInput = queryByTestId(rendered, 'username-input');
+        const passwordInput = queryByTestId(rendered, 'password-input');
+        const loginButton = queryByTestId(rendered, 'login-button');
+
+        typedAssertInstanceOf(usernameInput, HTMLInputElement);
+        typedAssertInstanceOf(passwordInput, HTMLInputElement);
+        typedAssertInstanceOf(loginButton, HTMLButtonElement);
+
+        return {
+            usernameInput,
+            passwordInput,
+            loginButton,
+            fullElement: rendered,
+        };
+    }
+
+    it('should has all the necessary children', async () => {
+        const elements = await getLoginFormElements();
+
+        assert.strictEqual(Object.keys(elements).length, 4);
+    });
+
+    it('should not allow logging in without data entry', async () => {
+        const elements = await getLoginFormElements();
+
+        const fullInitialTextContent = elements.fullElement.shadowRoot?.textContent;
+
+        assertNotNullish(fullInitialTextContent);
+        assert.isNotEmpty(fullInitialTextContent);
+
+        await clickElement(elements.loginButton);
+    });
+});
---
+++ packages/challenges/src/challenges/vir-2022-08-05.element.ts
@@ -0,0 +1,9 @@
+import {defineFunctionalElement, html} from 'element-vir';
+
+export const Vir_2022_08_05 = defineFunctionalElement({
+    tagName: 'vir-2022-08-05',
+    props: {},
+    renderCallback: () => {
+        return html``;
+    },
+});
