02e72c29d2f914bc3f2430bbde9ea60fc7e46661
electrovir <electrovir@users.noreply.github.com>
2022-08-18 20:22:29 +0000

    update template

--- packages/challenges/src/vir-put-date-here.element.test.ts
+++ packages/challenges/src/vir-put-date-here.element.test.ts
@@ -1,11 +1,18 @@
 import {fixture as renderFixture} from '@open-wc/testing';
 import {html} from 'element-vir';
+import {typedAssertInstanceOf} from './test-helpers/typed-assert';
 import {Vir_put_date_here} from './vir-put-date-here.element';
 
 describe(Vir_put_date_here.tagName, () => {
-    it('should something', async () => {
+    async function renderElement() {
         const rendered = await renderFixture(html`
             <${Vir_put_date_here}></${Vir_put_date_here}>
         `);
+        typedAssertInstanceOf(rendered, HTMLElement);
+        return rendered;
+    }
+
+    it('should render an element', async () => {
+        await renderElement();
     });
 });
